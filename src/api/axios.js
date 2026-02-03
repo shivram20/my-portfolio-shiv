7687eab654eb17d47b0d69f67e688d0cb3617640
shivram20 <shivramvasava20@gmail.com>
2026-02-03 18:13:56 +0530

    change api code

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,23 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_URL="https://my-portfolio-api-pspz.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 export default api;
-
-
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL,
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// });
-
-// export default api;
